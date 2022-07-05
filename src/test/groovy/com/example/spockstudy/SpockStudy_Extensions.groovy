a6a32acd14240dedad32fad383616bb1184ff4ad
kangjoshi <kangojshi@gmail.com>
2022-07-05 21:55:20 +0900

    Extensions 페이지 공부

--- src/test/groovy/com/example/spockstudy/SpockStudy_Extensions.groovy
+++ src/test/groovy/com/example/spockstudy/SpockStudy_Extensions.groovy
@@ -1,10 +1,14 @@
 package com.example.spockstudy
 
 import spock.lang.Ignore
-import spock.lang.PendingFeature
 import spock.lang.PendingFeatureIf
 import spock.lang.Requires
+import spock.lang.Retry
 import spock.lang.Specification
+import spock.lang.TempDir
+import spock.lang.Timeout
+
+import java.util.concurrent.TimeUnit
 
 // https://spockframework.org/spock/docs/2.1/extensions.html
 class SpockStudy_Extensions extends Specification {
@@ -36,6 +40,21 @@ class SpockStudy_Extensions extends Specification {
      *   - 기능이 아직 구현이 되지 않아 오류 보고에 포함되지 않아야 함을 나타 냄
      *   - @PendingFeatureIf : 조건 추가
      *
+     *  @Stepwise
+     *   - 클래스 붙이면, 테스트가 선언된 순서대로 수행된다
+     *   - 해당 어노테이션이 붙은 클래스에만 적용, 상속을 통한 적용은 되지 않음
+     *
+     *  @Timeout
+     *   - 주어진 시간을 초과하면 실패 발생
+     *   - 기본 : 초, unit 속성으로 조정 가능
+     *
+     *  @Retry
+     *   - 실패 가능성이 있는 원격 테스트를 위해 사용
+     *   - count (기본값 3), exceptions, condition등 설정 가능
+     *
+     *  @TempDir
+     *   - 임시 폴더 생성, 테스트 종료 후에는 삭제 된다
+     *
      * */
 
     @Ignore("임시로 테스트 수행 금지")
@@ -56,6 +75,21 @@ class SpockStudy_Extensions extends Specification {
     def "It will not support on Windows"() {
 
     }
+
+    @Retry(count = 5)
+    @Timeout(5)
+    def "fail if run for more then five seconds"() {
+
+    }
+
+    @Retry(exceptions = IOException)
+    @Timeout(value = 10, unit = TimeUnit.MILLISECONDS)
+    def "fail if run for more then ten milliseconds"() {
+
+    }
+
+    @TempDir
+    File tempDir
 
 
 
